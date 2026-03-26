c894ae8e2cfa46530aa6af09701b6f8900be032d
Danielodas <154921587+Danielodas@users.noreply.github.com>
2026-03-26 23:37:25 +0100

    ADD: functional app

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+mod steam_apps;
+
+use std::io;
+use steam_apps::SteamApp;
+
+#[tokio::main]
+async fn main() {
+    // TODO: let the user decide if they want to analyze it on Windows, Linux, or just get a name from an AppId
+
+    println!("Select what you want to do:");
+    println!("========================================================");
+    println!("1. Get Steam app name from id");
+    println!("2. Analyze Steam workshop size (Windows)");
+    println!("3. Analyze Steam workshop size (Linux) (not implemented)");
+    println!("========================================================");
+    let mut option = String::new();
+    io::stdin().read_line(&mut option).unwrap();
+
+    match option.trim() {
+        "1" => {
+            println!("Please, enter de app id:");
+            let mut app_id = String::new();
+            io::stdin().read_line(&mut app_id).unwrap();
+
+            let steam_app: SteamApp = SteamApp::new(app_id.clone());
+            let app_name = steam_app.id_to_name().await;
+
+            print!("ID: {} - Name: {}", app_id.trim(), app_name);
+        }
+        "2" => {
+            println!();
+            SteamApp::get_workshop_storage("W".to_string()).await;
+        }
+        "3" => {
+            println!();
+            SteamApp::get_workshop_storage("L".to_string()).await;
+        }
+        _ => println!("Option out of scope."),
+    }
+
+    println!("\n\nPress enter to exit...");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+}
---
+++ src/steam_apps.rs
@@ -0,0 +1,89 @@
+use reqwest;
+use std::fs;
+use std::path::Path;
+
+pub struct SteamApp {
+    app_id: String,
+}
+
+impl SteamApp {
+    pub fn new(app_id: String) -> SteamApp {
+        SteamApp { app_id }
+    }
+
+    pub async fn id_to_name(&self) -> String {
+        let url = format!("https://store.steampowered.com/api/appdetails?appids={}", self.app_id);
+        
+        let response = reqwest::get(&url).await;
+
+        match response {
+            Ok(resp) => {
+                let json: serde_json::Value = resp.json().await.unwrap();
+
+                let name: &str = json[&self.app_id]["data"]["name"]
+                    .as_str()
+                    .unwrap_or("Unknown");
+
+                name.to_string()
+            }
+            Err(_) => "Fetch error.".to_string(),
+        }
+    }
+
+    pub async fn get_workshop_storage(os: String) -> f64 {
+        let workshop_path: &Path;
+        let mut total_size: f64 = 0.0;
+
+        if os == "W" {
+            workshop_path = Path::new("C:/Program Files (x86)/Steam/steamapps/workshop/content");
+        }
+        else {
+            println!("Not supported.");
+            workshop_path = Path::new("C:/Program Files (x86)/Steam/steamapps/workshop/content");
+        }
+
+        if !workshop_path.exists() {
+            print!("Directory not found.");
+            return 0.0;
+        }
+
+        println!("Analyzing {}...", workshop_path.display());
+        println!("=============================");
+
+        for entry in fs::read_dir(workshop_path).unwrap() {
+            let entry = entry.unwrap();
+            let size= get_directory_size(&entry.path());
+            let size_mb = size as f64 / 1_048_576.0;
+
+            let app_id = entry.file_name().to_string_lossy().to_string();
+            let app = SteamApp::new(app_id);
+            let name = app.id_to_name().await;
+            
+            total_size += size_mb;
+            println!("{} - {:.2} mb", name, size_mb)
+        }
+
+        println!("=============================");
+        print!("Total size: {:.2} mb", total_size);
+        total_size
+    }
+
+}
+
+fn get_directory_size(path: &Path) -> u64 {
+    let mut total: u64 = 0;
+
+    for entry in fs::read_dir(path).unwrap() {
+        let entry = entry.unwrap();
+        let metadata = entry.metadata().unwrap();
+
+        if metadata.is_file() {
+            total += metadata.len();
+        }
+        else if metadata.is_dir() {
+            total += get_directory_size(&entry.path());
+        }
+    }
+        
+    total
+}
